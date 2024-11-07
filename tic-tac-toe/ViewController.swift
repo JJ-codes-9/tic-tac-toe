1764c28247594b95e1dc0cdae091d71826a5853d
JJ-codes-9 <154470908+JJ-codes-9@users.noreply.github.com>
2024-11-06 23:04:17 -0500

    Initial Commit

---
+++ tic-tac-toe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  tic-tac-toe
+//
+//  Created by Ceyda Bulat on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var grid: UIImageView!
+    
+    @IBAction func playerO(_ sender: UIButton) {
+     
+    }
+    
+    @IBAction func playerX(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
