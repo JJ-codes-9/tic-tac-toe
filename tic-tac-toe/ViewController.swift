0550355f994ed91699984471b8d71d77e1cb18e4
JJ-codes-9 <154470908+JJ-codes-9@users.noreply.github.com>
2025-04-04 23:20:20 -0400

    Initial player turn logic and board setup
    - Added Player enum with marker and turn switching
    - Created shared button tap IBAction
    - Displayed player marker on valid move
    - Prevented overwriting filled buttons
    - Added border styling to game board buttons

--- tic-tac-toe/ViewController.swift
+++ tic-tac-toe/ViewController.swift
@@ -6,57 +6,35 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
 
-    // The outlet for the grid image
-    @IBOutlet var grid: UIImageView!
-    
-    //Player buttons to show whose turn it is
-    
-    @IBOutlet var playerXButton: UIButton!
-    @IBOutlet var playerOButton: UIButton!
-    
-    
-    // The collection of each box as an array
-    @IBOutlet var boxes: [UIButton]!
-    
-    // The TicTacToe board instance
-    var board = TicTacToeBoard()
-    
-    // Track the current player (starting with Player X)
-    var currentPlayer: TicTacToeBoard.Box = .playerX
-    
-    
+    // Tracks whose turn it is (starts with Player X)
+    var currentPlayer: Player = .playerX
+
+    // Called whenever a button on the board is tapped
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // Check if the button is currently empty
+        if sender.title(for: .normal) == "" {
+            // Set the tapped button’s title to the current player's marker ("X" or "O")
+            sender.setTitle(currentPlayer.marker, for: .normal)
+
+            // Switch to the next player
+            currentPlayer = currentPlayer.next
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Testing ActivePlayer - remove later
-        let activePlayer = ActivePlayer(player: .playerX)
-        print("Current player: \(activePlayer.player.rawValue)")
-        print("Next player: \(activePlayer.nextPlayer.rawValue)")
-    }
-
-    // Handle when a box is tapped
-    @IBAction func boxTapped(_ sender: UIButton) {
-        // Get the index of the tapped box
-        guard let index = boxes.firstIndex(of: sender) else { return }
-        
-        // Check if the box is empty
-        if board.play(at: index, for: currentPlayer) {
-            // Box is empty: Update UI and switch players
-            sender.setTitle(currentPlayer.rawValue, for: .normal)
-            currentPlayer = (currentPlayer == .playerX) ? .playerO : .playerX
-        } else {
-            // Box is occupied: Show an alert
-            showAlert()
+        // Add border styling to all 9 buttons using their tags (1 to 9)
+        for tag in 1...9 {
+            if let button = view.viewWithTag(tag) as? UIButton {
+                button.layer.borderWidth = 1
+                button.layer.borderColor = UIColor.black.cgColor
+            }
         }
-    }
-    
-    // Show an alert when the box is already occupied
-    func showAlert() {
-        let alert = UIAlertController(title: "Invalid Move", message: "This box is already occupied. Choose another box.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
     }
 }
